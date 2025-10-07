29ffd590badc5d6d2cf8dd460653639f049554a6
Scott-K22 <s1160328@student.windesheim.nl>
2025-10-07 14:05:59 +0200

    Update PixelFuncs.cs
    finished assignment

--- BAI2/PixelFuncs.cs
+++ BAI2/PixelFuncs.cs
@@ -11,7 +11,7 @@
         {
             // *** IMPLEMENTATION HERE *** //
 
-            return pixel & 0xFF00FFFF;
+            return pixel & 0xFF00FFFF; // vergelijkt de pixel code met de hexacode die ernaast staat, kijkt welke gelijk zijn en gelijke bits blijven hetzelfde en de andere worden 0
         }
 
         public static uint FilterGroen(uint pixel)
@@ -30,7 +30,7 @@
         public static byte RoodWaarde(uint pixelvalue)
         {
             // *** IMPLEMENTATION HERE *** //
-            return (byte)((pixelvalue >> 16) & 0xFF);
+            return (byte)((pixelvalue >> 16) & 0xFF); // shift (in dit geval de rode bits) de pixelvalue naar het begin en vergelijkt met de hexacode die ernaast staat, zodat er alleen een rode waarde uit komt (shift 16 keer omdat rood (23-15 bits) 16 bits van 7-0 vandaan is)
         }
 
         public static byte GroenWaarde(uint pixelvalue)
@@ -48,7 +48,15 @@
         public static uint Steganografie(uint pixelvalue)
         {
             // *** IMPLEMENTATION HERE *** //
-            return 0;
+
+            // plaatje zit verstopt in rode bits, dus shift de rode bits naar 7-0 bits en vergelijkt dat met 1, omdat we alleen de minst-significate bit willen hebben
+            uint redBit = (pixelvalue >> 16) & 1;
+
+            // als de bit 1 is, wordt het rood, als de bit 0 is, wordt het zwart
+            uint newColor = redBit == 1 ? 0xFFFF0000 : 0xFF000000;
+
+            //returns newRed
+            return newColor;
         }
 
 
